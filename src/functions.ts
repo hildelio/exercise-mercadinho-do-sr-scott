aa540920dd7570fd38562b8347d43fc304675dba
hildelio <hildelio@gmail.com>
2023-06-23 10:42:05 -0300

    Mercadinho do Sr Scott :convenience_store: Req 04

--- src/functions.ts
+++ src/functions.ts
@@ -1,3 +1,14 @@
 export function first<T>(param: T[]): T {
   return param[0];
 }
+
+export function updateItem<T>(array: T[], index: number, newValue: T): T[] {
+  if (index < 0 || index >= array.length) {
+    return array;
+  }
+  return [
+    ...array.slice(0, index),
+    newValue,
+    ...array.slice(index + 1),
+  ];
+}
